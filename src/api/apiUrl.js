cca3c8b41dba5dad7068d2dcf08184d1e897cec6
杨浩 <yanghao@cdyoue.com>
2018-09-15 14:20:35 +0800

    添加资源管理API

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -27,10 +27,18 @@ export default {
   roleUpdate: { url: '/mgr/setRole1', method: 'post' }, //设置用户角色
 
   //资源管理
+  resourceAdd: { url: '/api/v2/daas/swap/resource/db', method: 'post' }, //新建资源实体
+  resourceConnect: { url: '/api/v2/daas/swap/resource/connect', method: 'get'}, //连接测试
+  resourceList: { url: '/api/v2/daas/swap/resource/itemList', method: 'get' }, //资源实体列表
+  resourceDelete: { url: '/api/v2/daas/swap/resource/item', method: 'delete' }, //删除资源实体
+  //catalogDetail: { url: '/api/v2/daas/swap/resource', method: 'get' }, //资源详情
+  resourceUpdate: { url: '/api/v2/daas/swap/resource/db', method: 'put' }, //修改资源
+  resourceMysqlColumn: { url: '/api/v2/daas/swap/resource/mysql/column', method: 'get' }, //获取mysql指定库表的字段list
+  resourceMysqlDb: { url: '/api/v2/daas/swap/resource/mysql/db', method: 'get' }, //获取mysql的数据库list
+  resourceMysqlTable: { url: '/api/v2/daas/swap/resource/mysql/table', method: 'get' }, //获取mysql指定库的数据表list
 
   //目录管理
   catalogAdd: { url: '/api/v2/daas/swap/resource', method: 'post' }, //新建资源
-  catalogConnect: { url: '/api/v2/daas/swap/resource/connect', method: 'get'}, //连接测试
   catalogList: { url: '/api/v2/daas/swap/resource/list', method: 'get' }, //资源列表
   catalogDelete: { url: '/api/v2/daas/swap/resource', method: 'delete' }, //删除资源
   catalogDetail: { url: '/api/v2/daas/swap/resource', method: 'get' }, //资源详情
@@ -38,7 +46,6 @@ export default {
   catalogItemsList: { url: '/api/v2/daas/swap/resource/{id}/infoList/', method: 'get' }, //获取资源信息项
   catalogShareDetail: { url: '/api/v2/daas/swap/resource/{id}/shareopen', method: 'get' }, //获取资源共享开放信息
   catalogShareUpdate: { url: '/api/v2/daas/swap/resource/{id}/shareopen', method: 'put' }, //修改资源共享开放信息
-
 
   //资源集市
   resourceBazaar: { url: '/api/v2/daas/swap/resourceBazaar/list', method: 'get' }, //获取资源集市列表
