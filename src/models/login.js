915e681edd25b2e8c01bb46bb9859686620831c3
杨浩 <yanghao@cdyoue.com>
2018-11-16 18:54:33 +0800

    完成mysql接入修改

--- src/models/login.js
+++ src/models/login.js
@@ -75,7 +75,7 @@ export default {
     changeLoginStatus(state, { payload }) {
       let auth;
       if (payload.result && payload.result !== '') {
-        auth = payload.result.data.roleEnNames[0];
+        auth = payload.result.data.roleEnNames[1];
       } else {
         auth = payload.currentAuthority;
       }
